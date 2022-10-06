dbf6dc44988dea6d1aca84dc668ea94fcc5ed949
Pengyy <1754148674@qq.com>
2022-10-06 21:53:53 +0800

    hot-fix commit1

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello.word");
         System.out.println("hello.word2");
         System.out.println("hello.word3");
+        System.out.println("hello.word4");
     }
 }
