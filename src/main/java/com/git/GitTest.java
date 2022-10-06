b870ba6183a68f066bd05665f0f9926135bcf1be
Pengyy <1754148674@qq.com>
2022-10-06 21:38:17 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.git;
+
+/**
+ * @program: sc220501
+ * @description:
+ * @author: Pengyangyang
+ * @create: 2022-10-06 21:31
+ * @version:1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.word");
+    }
+}
